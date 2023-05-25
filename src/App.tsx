9161860b08d988332bcb3d2df74ddf9e490b15bf
xushuxiang <v_xushuxiang@bilibili.com>
2023-05-25 15:35:53 +0800

    fix(close issues): test close issues
    fix #2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const a = '1232111'
+  const a = '12321111'
   return (
     <div className="App">
       <header className="App-header">
