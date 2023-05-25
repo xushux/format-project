4d5776ac8a53935be88b55962c5a3874271b84aa
xushuxiang <v_xushuxiang@bilibili.com>
2023-05-25 16:17:33 +0800

    xxxxx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const a = '12321111'
+  const a = '1231'
   return (
     <div className="App">
       <header className="App-header">
